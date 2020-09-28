c8159396ce3ff4da0ce5889e68313bcaf7606bb9
Toyrobs <toyinrobinson@Toybot02.localdomain>
2020-09-28 19:27:29 -0400

    new thangs

--- food4you/src/App.js
+++ food4you/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 function App() {
+  const [location, setLocation] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setLocation(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(location);
+
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
-   
+    <div className="container">
+      <h1>My Location</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Where's your location?"
+          value={location}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      <h2>{headingText}</h2>
     </div>
   );
 }
