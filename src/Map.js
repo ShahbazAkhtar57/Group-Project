d12901dc51771dd0a82ac131ad5c7e7053e4e3f3
Toyrobs <toyinrobinson@Toybot02.localdomain>
2020-10-22 13:35:10 -0400

    drag code changed back

--- src/Map.js
+++ src/Map.js
@@ -62,16 +62,11 @@ onMarkerDragEnd=(event)=>
 
         Geocode.fromLatLng(newLat, newLng).then(
             response => {
-                const address = response.results[0].formatted_address,
-                    addressArray = response.results[0].address_components,
-                    city = this.getCity(addressArray),
-                    area = this.getArea(addressArray),
-                    state = this.getState(addressArray);
+                const address = response.results[0].formatted_address;
+                  
                 this.setState({
-                    address: (address) ? address : '',
-                    area: (area) ? area : '',
-                    city: (city) ? city : '',
-                    state: (state) ? state : '',
+                   address: (address) ? address : '',
+                 
                     markerPosition: {
                         lat: newLat,
                         lng: newLng
@@ -86,41 +81,6 @@ onMarkerDragEnd=(event)=>
                 console.error(error);
             }
         );
-    };
-      getCity = (addressArray) => {
-        let city = '';
-        for (let i = 0; i < addressArray.length; i++) {
-            if (addressArray[i].types[0] && 'administrative_area_level_2' === addressArray[i].types[0]) {
-                city = addressArray[i].long_name;
-                return city;
-            }
-        }
-    };
-
-    getArea = (addressArray) => {
-        let area = '';
-        for (let i = 0; i < addressArray.length; i++) {
-            if (addressArray[i].types[0]) {
-                for (let j = 0; j < addressArray[i].types.length; j++) {
-                    if ('sublocality_level_1' === addressArray[i].types[j] || 'locality' === addressArray[i].types[j]) {
-                        area = addressArray[i].long_name;
-                        return area;
-                    }
-                }
-            }
-        }
-    };
-
-    getState = (addressArray) => {
-        let state = '';
-        for (let i = 0; i < addressArray.length; i++) {
-            for (let i = 0; i < addressArray.length; i++) {
-                if (addressArray[i].types[0] && 'administrative_area_level_1' === addressArray[i].types[0]) {
-                    state = addressArray[i].long_name;
-                    return state;
-                }
-            }
-        }
     };
 
 
