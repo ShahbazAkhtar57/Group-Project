d39adf0dea9f05e3077dae3642340359ae785836
Wen Bo <32940044+wenbo624@users.noreply.github.com>
2020-10-22 11:07:36 -0400

    Change card format

--- src/restaurant.js
+++ src/restaurant.js
@@ -3,16 +3,25 @@ import React from "react";
 function Restaurant() {
     return (<div className = "locinfo">
        
-        <div class="left">
+        <div className="left">
             <h1>Restaurant Information:</h1>
+            <hr></hr>
+            <br></br>
+            <h2 style={{textAlign:"left"}}>Restaurant Name: </h2>
+            <br></br>
             <h2 style={{textAlign:"left"}}>Restaurant Location: </h2>
+            <br></br>
+            <h2 style={{textAlign:"left"}}>Phone Number: </h2>
+            <br></br>
+            <h2 style={{textAlign:"left"}}>Rating:  </h2>
+            <br></br>
+            <h2 style={{textAlign:"left"}}>Price: </h2>
+
+
         </div>
 
-        <div class="middle">
-            <h1>Restaurant Menu:</h1>
-        </div>
 
-        <div class="right">
+        <div className="right">
             <h1>Review/s:</h1>
         </div>
 
